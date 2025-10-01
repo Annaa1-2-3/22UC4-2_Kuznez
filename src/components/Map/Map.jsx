0441af8137ff657a90fca034b8f5dd63a5a6c503
Annaa1-2-3 <159067905+Annaa1-2-3@users.noreply.github.com>
2025-10-01 16:11:20 +0300

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,33 @@
-// src/components/Map/Map.jsx
 import React from 'react';
 import styles from './Map.module.css';
+import mapShchelyur from '../../assets/map.png';
+import mapVertep from '../../assets/map_vertep.png';
+import mapKrasnobor from '../../assets/map_krasnobor.png';
+import mapDnyor from '../../assets/map_dnyor.png';
 
-function Map() {
+function Map({ selectedLocation }) {
+  let mapImage;
+
+  switch (selectedLocation) {
+    case 'shchelyur':
+      mapImage = mapShchelyur;
+      break;
+    case 'vertep':
+      mapImage = mapVertep;
+      break;
+    case 'krasnobor':
+      mapImage = mapKrasnobor;
+      break;
+    case 'dnyor':
+      mapImage = mapDnyor;
+      break;
+    default:
+      mapImage = mapShchelyur;
+  }
+
   return (
     <div className={styles.map}>
-      <img src="https://via.placeholder.com/1440x300?text=Карта+магазинов" alt="Карта" />
+      <img src={mapImage} alt="Карта" className={styles.image} />
     </div>
   );
 }
